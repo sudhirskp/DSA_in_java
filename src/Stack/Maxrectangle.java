7764c62862500839076ce8cff6efa80b5972e7b2
sudhirskp <gnitwallah@gmail.com>
2025-03-31 12:48:41 +0530

    histogram - leetcode 84

--- src/Stack/Maxrectangle.java
+++ src/Stack/Maxrectangle.java
@@ -55,27 +55,49 @@ public class Maxrectangle {
 
     //--------------------------------------------------------------------------------
     //2nd method
-//    public int largestRectangleArea(int[] heights) {
-//        Stack<Integer> st = new Stack<>();
-//        int n = heights.length;
-//        int max = Integer.MIN_VALUE;
-//        for(int i=0;i<=n;i++){
-//            int ele = (i==n)?0:heights[i];
-//            while(!st.isEmpty() && heights[st.peek()]>ele){
-//                int h = heights[st.pop()];
-//                int ps = st.isEmpty() ? -1 : st.peek();
-//                int w = i-ps-1;
-//                max = Math.max(max , h*w);
-//            }
-//            st.push(i);
-//        }
-//        return (max == Integer.MIN_VALUE) ? 0 : max;
-//    }
+    public int largestRectangleArea2(int[] heights) {
+        Stack<Integer> st = new Stack<>();
+        int n = heights.length;
+        int max = Integer.MIN_VALUE;
+        for(int i=0;i<=n;i++){
+            int ele = (i==n)?0:heights[i];
+            while(!st.isEmpty() && heights[st.peek()]>ele){
+                int h = heights[st.pop()];
+                int ps = st.isEmpty() ? -1 : st.peek();
+                int w = i-ps-1;
+                max = Math.max(max , h*w);
+            }
+            st.push(i);
+        }
+        return (max == Integer.MIN_VALUE) ? 0 : max;
+    }
 //------------------------------------------------------------------------------------------
+
+    //by using custome stack 99% faster
+    public int largestRectangleArea3(int[] heights) {
+        int n = heights.length;
+        int st [] = new int[n+1];
+        int index =-1;
+        int max = Integer.MIN_VALUE;
+        for(int i=0;i<=n;i++){
+            int ele = (i==n)?0:heights[i];
+            while(index!=-1 && heights[st[index]]>ele){
+                int h = heights[st[index--]];
+                int ps = index==-1 ? -1 : st[index];
+                int w = i-ps-1;
+                max = Math.max(max , h*w);
+            }
+            st[++index]=i;
+        }
+        return (max == Integer.MIN_VALUE) ? 0 : max;
+    }
+
+    //-----------------------------
     public static void main(String[] args) {
         int [] arr = {2,1,5,6,2,3};
         Maxrectangle m = new Maxrectangle();
         //System.out.println(m.largestRectangleArea(arr));
-        System.out.println(m.largestRectangleArea(arr));
+        System.out.println(m.largestRectangleArea2(arr));
+        System.out.println(m.largestRectangleArea3(arr));
     }
 }
