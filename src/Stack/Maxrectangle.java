f12bc1ba712f27c13f84f0ce0194e9f7e0d54ac3
sudhirskp <gnitwallah@gmail.com>
2025-03-31 12:28:32 +0530

    histogram - leetcode 84

--- src/Stack/Maxrectangle.java
+++ src/Stack/Maxrectangle.java
@@ -53,6 +53,25 @@ public class Maxrectangle {
         return maxArea;
     }
 
+    //--------------------------------------------------------------------------------
+    //2nd method
+//    public int largestRectangleArea(int[] heights) {
+//        Stack<Integer> st = new Stack<>();
+//        int n = heights.length;
+//        int max = Integer.MIN_VALUE;
+//        for(int i=0;i<=n;i++){
+//            int ele = (i==n)?0:heights[i];
+//            while(!st.isEmpty() && heights[st.peek()]>ele){
+//                int h = heights[st.pop()];
+//                int ps = st.isEmpty() ? -1 : st.peek();
+//                int w = i-ps-1;
+//                max = Math.max(max , h*w);
+//            }
+//            st.push(i);
+//        }
+//        return (max == Integer.MIN_VALUE) ? 0 : max;
+//    }
+//------------------------------------------------------------------------------------------
     public static void main(String[] args) {
         int [] arr = {2,1,5,6,2,3};
         Maxrectangle m = new Maxrectangle();
