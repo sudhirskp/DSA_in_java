b612e77be45cdaea98a4abcdc3091b7274237f65
sudhirskp <gnitwallah@gmail.com>
2025-06-28 13:31:37 +0530

    Add orangesRotting2 method to calculate time for all oranges to rot in a grid

--- src/Pattern/BFS/RottingOrange.java
+++ src/Pattern/BFS/RottingOrange.java
@@ -63,6 +63,57 @@ public class RottingOrange {
             return ans;
         }
 
+    public int orangesRotting2(int[][] grid) {
+        int row = grid.length;
+        int col = grid[0].length;
+        int freshCount = 0;
+        Queue<int[]> que = new LinkedList<>();
+
+        for (int i = 0; i < row; i++) {
+            for (int j = 0; j < col; j++) {
+                if (grid[i][j] == 2) {
+                    que.offer(new int[] { i, j });
+                }
+
+                if (grid[i][j] == 1) {
+                    freshCount++;
+                }
+            }
+        }
+
+        if (freshCount == 0) {
+            return 0;
+        }
+
+        int time = 0;
+        while (!que.isEmpty()) {
+            int size = que.size();
+            for (int i = 0; i < size; i++) {
+                int[] index = que.poll();
+                int r = index[0];
+                int c = index[1];
+                int[][] dir = { { 1, 0 }, { 0, 1 }, { -1, 0 }, { 0, -1 } };
+
+                for (int[] ind : dir) {
+                    int x = r + ind[0];
+                    int y = c + ind[1];
+
+                    if (x >= 0 && x < row && y >= 0 && y < col && grid[x][y] == 1 && grid[x][y] != 0) {
+                        grid[x][y] = 2;
+                        que.offer(new int[] { x, y });
+                        freshCount--;
+                    }
+                }
+            }
+            time++;
+            if (freshCount == 0)
+                break;
+        }
+
+        return (freshCount == 0) ? time : -1;
+    }
+
+
     public static void main(String[] args) {
             RottingOrange obj = new RottingOrange();
             int[][] grid = {
@@ -70,7 +121,7 @@ public class RottingOrange {
                     {1, 1, 0},
                     {0, 1, 1}
             };
-            int result = obj.orangesRotting(grid);
+            int result = obj.orangesRotting2(grid);
             System.out.println("Time taken for all oranges to rot: " + result);
     }
 }
