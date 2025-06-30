072074ae949a79cae75cb05d886db38a9beb57e6
sudhirskp <gnitwallah@gmail.com>
2025-06-30 22:18:48 +0530

    Add numIslands3 method to No_of_Island class for counting islands using BFS

--- src/Pattern/Matrix_Traversal/No_of_Island.java
+++ src/Pattern/Matrix_Traversal/No_of_Island.java
@@ -1,5 +1,8 @@
 package Pattern.Matrix_Traversal;
 
+import java.util.LinkedList;
+import java.util.Queue;
+
 public class No_of_Island {
 
     int row, col;
@@ -65,6 +68,38 @@ public class No_of_Island {
 
     }
 
+    //3rd approach - 7ms
+    public int numIslands3(char[][] grid) {
+         row = grid.length;
+         col = grid[0].length;
+        int count = 0;
+        Queue<int[]> que = new LinkedList<>();
+        for (int i = 0; i < row; i++) {
+            for (int j = 0; j < col; j++) {
+                if (grid[i][j] == '1') {
+                    count++;
+                    que.offer(new int[] { i, j });
+                    grid[i][j] = '0';
+                    int[][] dir = { { 1, 0 }, { -1, 0 }, { 0, -1 }, { 0, 1 } };
+                    while (!que.isEmpty()) {
+                        int[] index = que.poll();
+                        for (int[] ind : dir) {
+                            int x = index[0] + ind[0];
+                            int y = index[1] + ind[1];
+                            if (x >= 0 && x < row && y >= 0 && y < col && grid[x][y] != '0') {
+                                que.offer(new int[] { x, y });
+                                grid[x][y] = '0';
+                            }
+                        }
+
+                    }
+                }
+            }
+        }
+        return count;
+    }
+
+
     public static void main(String[] args) {
         char[][] grid = {
                 { '1', '1', '0', '0', '0' },
@@ -73,7 +108,7 @@ public class No_of_Island {
                 { '0', '0', '0', '1', '1' }
         };
         No_of_Island obj = new No_of_Island();
-        System.out.println(obj.numIslands(grid));
+        System.out.println(obj.numIslands3(grid));
 
     }
 }
