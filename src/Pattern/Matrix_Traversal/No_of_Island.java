284d841855b86885c393faa6aba7f6c0eea7093f
sudhirskp <gnitwallah@gmail.com>
2025-06-30 21:44:21 +0530

    Add numIslands2 method to No_of_Island class for counting islands using DFS

--- src/Pattern/Matrix_Traversal/No_of_Island.java
+++ src/Pattern/Matrix_Traversal/No_of_Island.java
@@ -35,6 +35,36 @@ public class No_of_Island {
 
     }
 
+    //2nd approach - 4ms
+    public int numIslands2(char[][] grid) {
+        row = grid.length;
+        col = grid[0].length;
+        int count = 0;
+        for (int i = 0; i < row; i++) {
+            for (int j = 0; j < col; j++) {
+                if (grid[i][j] == '1') {
+                    dfs(i, j, grid);
+                    count++;
+                }
+            }
+        }
+        return count;
+    }
+
+    public void dfs(int r, int c, char[][] grid) {
+        grid[r][c] = '0';
+        int[][] dir = { { r + 1, c }, { r - 1, c }, { r, c + 1 }, { r, c - 1 } };
+        for (int[] neigh : dir) {
+            int x = neigh[0];
+            int y = neigh[1];
+            if (x >= 0 && x < row && y >= 0 && y < col && grid[x][y] != '0') {
+                dfs(x, y, grid);
+            }
+
+        }
+
+    }
+
     public static void main(String[] args) {
         char[][] grid = {
                 { '1', '1', '0', '0', '0' },
