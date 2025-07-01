d45bad7b43ef8926c171d0a36f8945ab5e032ad4
sudhirskp <gnitwallah@gmail.com>
2025-07-01 21:44:00 +0530

    Add solve1 method and edge1 helper for surrounded regions problem using BFS

--- src/Pattern/Matrix_Traversal/surroundedRegions.java
+++ src/Pattern/Matrix_Traversal/surroundedRegions.java
@@ -1,5 +1,8 @@
 package Pattern.Matrix_Traversal;
 
+import java.util.LinkedList;
+import java.util.Queue;
+
 public class surroundedRegions {
 
     int row, col;
@@ -40,12 +43,65 @@ public class surroundedRegions {
         }
 
     }
+
+    public void solve1(char[][] board) {
+        row = board.length;
+        col = board[0].length;
+        for (int i = 0; i < row; i++) {
+
+            edge1(i, 0, board);
+            edge1(i, col - 1, board);
+
+        }
+
+        for (int j = 0; j < col; j++) {
+            edge1(0, j, board);
+            edge1(row - 1, j, board);
+        }
+
+        for (int i = 0; i < row; i++) {
+            for (int j = 0; j < col; j++) {
+                if (board[i][j] == 'O')
+                    board[i][j] = 'X';
+                else if (board[i][j] == 'E')
+                    board[i][j] = 'O';
+            }
+        }
+    }
+
+    public void edge1(int r, int c, char[][] grid) {
+        if (r < 0 || r >= row || c < 0 || c >= col || grid[r][c] != 'O') {
+            return;
+        }
+
+        Queue<int[]> queue = new LinkedList<>();
+        queue.offer(new int[] { r, c });
+
+        while (!queue.isEmpty()) {
+            int[] index = queue.poll();
+            int i = index[0], j = index[1];
+            grid[i][j] = 'E';
+
+            int[][] directions = { { 1, 0 }, { -1, 0 }, { 0, 1 }, { 0, -1 } };
+
+            for (int[] d : directions) {
+                int x = i + d[0];
+                int y = j + d[1];
+
+                if (x >= 0 && x < row && y >= 0 && y < col && grid[x][y] == 'O') {
+                    queue.offer(new int[] { x, y });
+                    grid[x][y]='E';
+                }
+            }
+        }
+    }
+
     public static void main(String[] args) {
 
         char[][] grid = { { 'X', 'X', 'X', 'X' }, { 'X', 'O', 'O', 'X' }, { 'X', 'X', 'O', 'X' },
                 { 'X', 'O', 'X', 'X' } };
         surroundedRegions obj = new surroundedRegions();
-        obj.solve(grid);
+        obj.solve1(grid);
         for (int i = 0; i < grid.length; i++) {
             for (int j = 0; j < grid[0].length; j++) {
                 System.out.print(grid[i][j] + " ");
