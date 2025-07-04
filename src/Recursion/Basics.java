918f3fee227db3beacc40bc5b82cbcb761e8913d
sudhirskp <gnitwallah@gmail.com>
2025-07-04 21:51:58 +0530

    Add printFirst method to Basics class for finding the first index of a target in an array

--- src/Recursion/Basics.java
+++ src/Recursion/Basics.java
@@ -18,10 +18,22 @@ public class Basics {
         printArr(arr , size -1);
         System.out.print(arr[size] + " ");
     }
+
+    //print first index of the target number of the arr
+    public static int printFirst(int arr[] , int target , int start){
+        if(start== arr.length) return -1;
+        if(arr[start]==target) return start;
+        else{
+            int index = printFirst(arr,target,start+1);
+            return index;
+        }
+    }
     public static void main(String[] args) {
 
         //System.out.println(Sum(5));
        // printIncreasing(5);
         printArr(new int [] {1,3,4,5,6,7} , 5);
+        System.out.println();
+        System.out.println(printFirst(new int []{2,3,4,5,6,4,3},4,0));
     }
 }
