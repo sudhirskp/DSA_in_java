35bc2d4c23e590656d15536fa547215f61c53b13
sudhirskp <gnitwallah@gmail.com>
2025-07-05 15:58:43 +0530

    Add bottom-up Fibonacci implementation and update main method for testing

--- src/DP/fibo.java
+++ src/DP/fibo.java
@@ -11,11 +11,31 @@ public class fibo {
         dp[n] = x+y;
         return x+y;
     }
-    public static void main(String[] args) {
-        int n = 5;
+
+    //bottom-up Approach
+
+    public static int fibb(int n){
         int [] dp = new int [n+1];
+        dp[0]=0;
+        dp[1]=1;
+        for(int i=2;i<dp.length;i++){
+            dp[i] = dp[i-1]+dp[i-2];
+        }
+
+        return dp[dp.length-1];
+    }
+    public static void main(String[] args) {
+        int n = 10;
+        int [] dp = new int [n+1];
+        System.out.println("By top Down : ");
         for(int i=1;i<=n;i++){
             System.out.print(fib(i,dp) + " ");
         }
+        System.out.println();
+        System.out.println("By Bottom Up : ");
+        for(int i=1;i<=n;i++){
+            System.out.print(fibb(i) + " ");
+        }
+
     }
 }
