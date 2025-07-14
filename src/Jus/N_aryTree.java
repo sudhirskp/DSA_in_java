4240b8d1edbfcad827f1fd855a2e9f14d2e75e2f
sudhirskp <gnitwallah@gmail.com>
2025-07-15 02:56:14 +0530

    juspay like que - n-ary tree depth

---
+++ src/Jus/N_aryTree.java
@@ -0,0 +1,38 @@
+package Jus;
+
+import java.util.List;
+
+public class N_aryTree {
+
+    class Node {
+        public int val;
+        public List<Node> children;
+
+        public Node() {}
+
+        public Node(int _val) {
+            val = _val;
+        }
+
+        public Node(int _val, List<Node> _children) {
+            val = _val;
+            children = _children;
+        }
+    }
+
+    public int maxDepth(Node root) {
+        if(root == null) return 0;
+        int max =0;
+        for(Node node : root.children){
+            int depth = maxDepth(node);
+            max = Math.max(max,depth);
+        }
+        return max+1;
+    }
+
+    public static void main(String[] args) {
+        N_aryTree ob = new N_aryTree();
+        //add node elements
+        System.out.println(ob.maxDepth(null));
+    }
+}
