89547187e860d60f5f90b66f160edc7e78833e66
sudhirskp <gnitwallah@gmail.com>
2025-07-15 02:57:03 +0530

    juspay like que - max depth

---
+++ src/Jus/DepthofTheTree.java
@@ -0,0 +1,42 @@
+package Jus;
+
+//not same but kind of
+
+class TreeNode {
+            int val;
+            TreeNode left;
+            TreeNode right;
+            TreeNode() {}
+            TreeNode(int val) { this.val = val; }
+            TreeNode(int val, TreeNode left, TreeNode right) {
+                    this.val = val;
+                    this.left = left;
+                    this.right = right;
+            }
+    }
+public class DepthofTheTree {
+
+
+
+    public int maxDepth(TreeNode root) {
+
+        if(root==null) return 0;
+
+        int left = maxDepth(root.left);
+        int right = maxDepth(root.right);
+
+        int length = Math.max(left,right);
+
+        return length+1;
+    }
+
+    public static void main(String[] args) {
+        DepthofTheTree d = new DepthofTheTree();
+        TreeNode root = new TreeNode(3);
+        root.left = new TreeNode(9);
+        root.right = new TreeNode(20);
+        root.right.left = new TreeNode(15);
+        root.right.right = new TreeNode(7);
+        System.out.println(d.maxDepth(root));
+    }
+}
