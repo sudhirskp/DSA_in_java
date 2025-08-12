b6e65a0118b49b6fd4a15ead98c3abb336901ca9
sudhirskp <gnitwallah@gmail.com>
2025-08-12 23:37:55 +0530

    N_Queens 51 - leetCode

---
+++ src/Recursion/N_Queens.java
@@ -0,0 +1,69 @@
+package Recursion;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class N_Queens {
+
+        public List<List<String>> solveNQueens(int n) {
+            List<List<String>> res = new ArrayList<>();
+            String[][] board = new String[n][n];
+
+            for (int i = 0; i < n; i++) {
+                Arrays.fill(board[i], ".");
+            }
+            findQ(n, res, 0, board);
+            return res;
+        }
+
+        private void findQ(int n, List<List<String>> res, int row, String[][] brd) {
+
+            if (row == n) {
+                if (row == n) {
+                    List<String> board = new ArrayList<>();
+                    for (int i = 0; i < n; i++) {
+                        board.add(String.join("", brd[i]));
+                    }
+                    res.add(board);
+                    return;
+                }
+
+            }
+            for (int col = 0; col < n; col++) {
+                if (Valid(row, col, n, brd)) {
+                    brd[row][col] = "Q";
+                    findQ(n, res, row + 1, brd);
+                    brd[row][col] = ".";
+                }
+            }
+        }
+
+        boolean Valid(int row, int col, int n, String [][] board) {
+            for (int i = 0; i < row; i++) {
+                if (board[i][col] == "Q")
+                    return false;
+            }
+
+            for (int i = row - 1, j = col - 1; i >= 0 && j >= 0; i--, j--) {
+                if (board[i][j] == "Q")
+                    return false;
+            }
+
+            for (int i = row - 1, j = col + 1; i >= 0 && j < n; i--, j++) {
+                if (board[i][j] == "Q")
+                    return false;
+            }
+            return true;
+        }
+
+    public static void main(String[] args) {
+        int n = 4;
+        N_Queens obj = new N_Queens();
+        List<List<String>> res = obj.solveNQueens(n);
+        System.out.println(res);
+
+    }
+}
+
+//leetcode 51
