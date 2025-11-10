04ede0ae2c3aaffb8c90529979c5df70becfaef2
sudhirskp <gnitwallah@gmail.com>
2025-11-11 01:11:51 +0530

    LeetCode 3542

---
+++ src/Daily_Task/Nov2025/AlleleTozero.java
@@ -0,0 +1,28 @@
+package Daily_Task.Nov2025;
+
+public class AlleleTozero {
+
+    public int minOperations(int[] nums) {
+        int [] stack = new int [nums.length];
+        int index =-1;
+        int ans =0;
+        for(int val : nums){
+            while(index!=-1 && stack[index]>val){
+                index--;
+            }
+            if(val > 0 && (index==-1 || val != stack[index])){
+                ans+=1;
+                stack[++index]=val;
+            }
+        }
+        return ans;
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
+//leetCode 3542
+//11 nov 2025
+//Time : O(n)
+//Space : O(n)
