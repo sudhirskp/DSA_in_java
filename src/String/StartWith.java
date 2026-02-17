5862956c600edc846763884218fb19eb83580cab
sudhirskp <gnitwallah@gmail.com>
2026-02-17 10:06:12 +0530

    refactor strStr method for improved readability and edge case handling

--- src/String/StartWith.java
+++ src/String/StartWith.java
@@ -2,26 +2,25 @@ package String;
 
 public class StartWith {
 
-        public int strStr(String haystack, String needle) {
-            int n = haystack.length();
-            int m = needle.length();
+    public int strStr(String haystack, String needle) {
+        int n = haystack.length();
+        int m = needle.length();
 
-            if (m == 0) return 0;  // edge case
-            if (m > n) return -1;
+        if (m == 0) return 0;
+        if (n < m) return -1;
 
-            for (int i = 0; i <= n - m; i++) {
-                if (haystack.substring(i, i + m).equals(needle)) {
-                    return i;  // found first occurrence
-                }
+        for (int i = 0; i <= n - m; i++) {
+            if (haystack.substring(i, i + m).equals(needle)) {
+                return i;
             }
-
-            return -1;  // not found
         }
 
+        return -1;
+    }
 
     public static void main(String[] args) {
         String haystack = "sadbutsad";
-        String needle = "sad";
+        String needle = "but";
         StartWith ob = new StartWith();
         System.out.println(ob.strStr(haystack, needle));
     }
