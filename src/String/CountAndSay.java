238f4f57b264b627bd04000e6ccb51f5f0cd132f
sudhirskp <gnitwallah@gmail.com>
2026-02-17 10:09:40 +0530

    add MapPair and newStr methods to enhance CountAndSay functionality

--- src/String/CountAndSay.java
+++ src/String/CountAndSay.java
@@ -1,5 +1,8 @@
 package String;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class CountAndSay {
 
     public String countAndSay(int n) {
@@ -7,6 +10,11 @@ public class CountAndSay {
         for (int i = 1; i < n; i++) {
             ans = buildNext(ans);
         }
+
+//        for (int i = 1; i < n; i++) {
+//            ans = newStr(MapPair(ans));
+//        }
+
         return ans;
     }
 
@@ -22,6 +30,32 @@ public class CountAndSay {
             }
         }
         sb.append(count).append(str.charAt(str.length() - 1));
+        return sb.toString();
+    }
+
+    private List<int[]> MapPair(String s) {
+        List<int[]> list = new ArrayList<>();
+        int count = 1;
+
+        for (int i = 1; i < s.length(); i++) {
+            if (s.charAt(i) == s.charAt(i - 1)) {
+                count++;
+            } else {
+                list.add(new int[] { s.charAt(i - 1) - '0', count });
+                count = 1;
+            }
+        }
+
+        list.add(new int[] { s.charAt(s.length() - 1) - '0', count });
+        return list;
+    }
+
+    private String newStr(List<int[]> list) {
+        StringBuilder sb = new StringBuilder();
+        for (int[] pair : list) {
+            sb.append(pair[1]);
+            sb.append(pair[0]);
+        }
         return sb.toString();
     }
 
