c10b204ea43cb88123b51632b24db91c9bfe4f8a
sudhirskp <gnitwallah@gmail.com>
2026-07-01 13:05:47 +0530

    add next pointer to Node class for enhanced tree traversal capabilities

--- src/Tree/BinaryTree/Node.java
+++ src/Tree/BinaryTree/Node.java
@@ -4,9 +4,18 @@ public class Node {
     int data;
     Node left;
     Node right;
+    Node next;
+
     Node(int data){
         this.data = data;
         this.left = null;
         this.right = null;
+    }
+
+    public Node(int _val, Node _left, Node _right, Node _next) {
+        data = _val;
+        left = _left;
+        right = _right;
+        next = _next;
     }
 }
